a4023a80d2afffcc90610b73ad420b7529d54521
Benjamin Moulin <bmoulin@matchbox.fr>
2016-10-07 18:22:57 +0200

    oc

--- app.js
+++ app.js
@@ -5,8 +5,16 @@ process.addListener("SIGINT", () => process.exit() )
 process.addListener("SIGTERM", () => process.exit() )
 
 const {DB} = require("./lib/db/DB")
-const db = new DB({ root: "./sample" })
+const db = new DB({ root: "/tudor/app/public" })
 
 db.addEventListener("readystatechange", ({readystate}) => {
     console.log("db readystate change =>", readystate)
 })
+
+/*
+db.query("/downloads/images/style/", (err, node) => {
+    console.log("yyy", err, node)
+}).catch(e => {
+    console.log("xxx")
+})
+*/
